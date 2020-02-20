2deb72365f5cc12634fd7a7baf058496702470bf
green512 <green512@163.com>
2020-02-20 09:45:15 +0800

    Add files via upload

---
+++ confirmed_xiaogan.js
@@ -0,0 +1,347 @@
+const LAST_UPDATE = "2020.02.20 09:38:00"; 
+const DATA = {
+  "420900": {
+    "confirmedCount": 3329,"cityName": "孝感市",  "deadCount": 89,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 835,"cityName": "孝南区",  "deadCount": 14,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 737, "cityName": "汉川市", "deadCount": 24,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 357, "cityName": "应城市",  "deadCount": 14,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 369, "cityName": "云梦县",  "deadCount": 9,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 502, "cityName": "安陆市", "deadCount": 14,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 292, "cityName": "大悟县",  "deadCount": 8,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 237, "cityName": "孝昌县",  "deadCount": 6,   "color": "#73181B"
+  }
+};
+const DATA0218 = {
+  "420900": {
+    "confirmedCount": 3344,"cityName": "孝感市",  "deadCount": 82,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 841,"cityName": "孝南区",  "deadCount": 14,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 734, "cityName": "汉川市", "deadCount": 23,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 359, "cityName": "应城市",  "deadCount": 11,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 368, "cityName": "云梦县",  "deadCount": 9,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 524, "cityName": "安陆市", "deadCount": 12,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 286, "cityName": "大悟县",  "deadCount": 8,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 232, "cityName": "孝昌县",  "deadCount": 5,   "color": "#73181B"
+  }
+};
+const DATA0217 = {
+  "420900": {
+    "confirmedCount": 3320,"cityName": "孝感市",  "deadCount": 75,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 834,"cityName": "孝南区",  "deadCount": 14,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 744, "cityName": "汉川市", "deadCount": 22,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 346, "cityName": "应城市",  "deadCount": 8,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 365, "cityName": "云梦县",  "deadCount": 8,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 515, "cityName": "安陆市", "deadCount": 12,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 285, "cityName": "大悟县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 231, "cityName": "孝昌县",  "deadCount": 5,   "color": "#73181B"
+  }
+};
+const DATA0216 = {
+  "420900": {
+    "confirmedCount": 3279,"cityName": "孝感市",  "deadCount": 70,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 830,"cityName": "孝南区",  "deadCount": 14,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 729, "cityName": "汉川市", "deadCount": 19,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 342, "cityName": "应城市",  "deadCount": 7,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 374, "cityName": "云梦县",  "deadCount": 8,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 492, "cityName": "安陆市", "deadCount": 12,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 284, "cityName": "大悟县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 228, "cityName": "孝昌县",  "deadCount": 4,   "color": "#73181B"
+  }
+};
+const DATA0215 = {
+  "420900": {
+    "confirmedCount": 3201,"cityName": "孝感市",  "deadCount": 65,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 810,"cityName": "孝南区",  "deadCount": 13,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 723, "cityName": "汉川市", "deadCount": 19,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 335, "cityName": "应城市",  "deadCount": 4,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 356, "cityName": "云梦县",  "deadCount": 8,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 477, "cityName": "安陆市", "deadCount": 11,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 274, "cityName": "大悟县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 226, "cityName": "孝昌县",  "deadCount": 4,   "color": "#73181B"
+  }
+};
+const DATA0214 = {
+  "420900": {
+    "confirmedCount": 3114,"cityName": "孝感市",  "deadCount": 62,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 785,"cityName": "孝南区",  "deadCount": 11,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 701, "cityName": "汉川市", "deadCount": 19,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 328, "cityName": "应城市",  "deadCount": 4,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 353, "cityName": "云梦县",  "deadCount": 8,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 466, "cityName": "安陆市", "deadCount": 10,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 261, "cityName": "大悟县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 220, "cityName": "孝昌县",  "deadCount": 4,   "color": "#73181B"
+  }
+};
+
+const DATA0213 = {
+  "420900": {
+    "confirmedCount": 3009,"cityName": "孝感市",  "deadCount": 57,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 760,"cityName": "孝南区",  "deadCount": 9,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 679, "cityName": "汉川市", "deadCount": 18,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 319, "cityName": "应城市",  "deadCount": 3,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 328, "cityName": "云梦县",  "deadCount": 7,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 454, "cityName": "安陆市", "deadCount": 10,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 252, "cityName": "大悟县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 217, "cityName": "孝昌县",  "deadCount": 4,   "color": "#73181B"
+  }
+};
+
+const DATA0212 = {
+  "420900": {
+    "confirmedCount": 2874,"cityName": "孝感市",  "deadCount": 49,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 729,"cityName": "孝南区",  "deadCount": 8,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 641, "cityName": "汉川市", "deadCount": 14,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 309, "cityName": "应城市",  "deadCount": 3,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 313, "cityName": "云梦县",  "deadCount": 7,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 441, "cityName": "安陆市", "deadCount": 8,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 238, "cityName": "大悟县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 203, "cityName": "孝昌县",  "deadCount": 3,   "color": "#73181B"
+  }
+};
+
+const DATA0211 = {
+  "420900": {
+    "confirmedCount": 2751,"cityName": "孝感市",  "deadCount": 45,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 694,"cityName": "孝南区",  "deadCount": 6,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 627, "cityName": "汉川市", "deadCount": 14,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 280, "cityName": "应城市",  "deadCount": 3,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 309, "cityName": "云梦县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 433, "cityName": "安陆市", "deadCount": 8,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 223, "cityName": "大悟县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 185, "cityName": "孝昌县",  "deadCount": 2,   "color": "#73181B"
+  }
+};
+
+
+const DATA0210 = {
+  "420900": {
+    "confirmedCount": 2642,"cityName": "孝感市",  "deadCount": 41,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 677,"cityName": "孝南区",  "deadCount": 5,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 592, "cityName": "汉川市", "deadCount": 14,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 269, "cityName": "应城市",  "deadCount": 3,   "color": "#73181B"
+  },
+  "420923": {
+    "confirmedCount": 291, "cityName": "云梦县",  "deadCount": 5,   "color": "#73181B"
+  },
+  "420982": {
+    "confirmedCount": 412, "cityName": "安陆市", "deadCount": 7,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 216, "cityName": "大悟县",  "deadCount": 6,   "color": "#73181B"
+  },
+  "420921": {
+    "confirmedCount": 185, "cityName": "孝昌县",  "deadCount": 1,   "color": "#73181B"
+  }
+};
+
+const DATA0209 = {
+  "420900": {
+    "confirmedCount": 2541,"cityName": "孝感市",  "deadCount": 33,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 630,"cityName": "孝南区",  "deadCount": 5,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 590, "cityName": "汉川市", "deadCount": 13,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 256, "cityName": "应城市",  "deadCount": 1,   "color": "#E04B49"
+  },
+  "420923": {
+    "confirmedCount": 277, "cityName": "云梦县",  "deadCount": 4,   "color": "#E04B49"
+  },
+  "420982": {
+    "confirmedCount": 399, "cityName": "安陆市", "deadCount": 6,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 206, "cityName": "大悟县",  "deadCount": 6,   "color": "#E04B49"
+  },
+  "420921": {
+    "confirmedCount": 183, "cityName": "孝昌县",  "deadCount": 0,   "color": "#E04B49"
+  }
+};
+
+const DATA0208 = {
+  "420900": {
+    "confirmedCount": 2436,"cityName": "孝感市",  "deadCount": 29,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 605,"cityName": "孝南区",  "deadCount": 5,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 574, "cityName": "汉川市", "deadCount": 13,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 250, "cityName": "应城市",  "deadCount": 0,   "color": "#E04B49"
+  },
+  "420923": {
+    "confirmedCount": 243, "cityName": "云梦县",  "deadCount": 4,   "color": "#E04B49"
+  },
+  "420982": {
+    "confirmedCount": 389, "cityName": "安陆市", "deadCount": 4,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 202, "cityName": "大悟县",  "deadCount": 3,   "color": "#E04B49"
+  },
+  "420921": {
+    "confirmedCount": 171, "cityName": "孝昌县",  "deadCount": 0,   "color": "#E04B49"
+  }
+};
+
+const DATA_0207 = {
+  "420900": {
+    "confirmedCount": 2313,"cityName": "孝感市",  "deadCount": 25,  "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 600,"cityName": "孝南区",  "deadCount": 5,    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 560, "cityName": "汉川市", "deadCount": 12,   "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 222, "cityName": "应城市",  "deadCount": 0,   "color": "#E04B49"
+  },
+  "420923": {
+    "confirmedCount": 237, "cityName": "云梦县",  "deadCount": 4,   "color": "#E04B49"
+  },
+  "420982": {
+    "confirmedCount": 365, "cityName": "安陆市", "deadCount": 4,   "color": "#73181B"
+  },
+  "420922": {
+    "confirmedCount": 173, "cityName": "大悟县",  "deadCount": 1,   "color": "#E04B49"
+  },
+  "420921": {
+    "confirmedCount": 156, "cityName": "孝昌县",  "deadCount": 0,   "color": "#E04B49"
+  }
+}
